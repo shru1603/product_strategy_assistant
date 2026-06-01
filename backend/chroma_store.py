c0bac5808dbea9d298dc017ccacdee7b037b01ad
Shruthi B <shruthi.b@prodapt.com>
2026-06-01 22:35:53 +0530

    use text-embedding-3-small for ChromaDB instead of default local model

--- backend/chroma_store.py
+++ backend/chroma_store.py
@@ -1,4 +1,5 @@
 import chromadb
+from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
 from langchain_openai import OpenAIEmbeddings
 from config import API_KEY, BASE_URL, EMBED_MODEL, CHROMA_PATH, get_http_client
 
@@ -11,13 +12,19 @@ embeddings = OpenAIEmbeddings(
 
 _client = chromadb.PersistentClient(path=CHROMA_PATH)
 
+_embedding_fn = OpenAIEmbeddingFunction(
+    api_key=API_KEY,
+    model_name=EMBED_MODEL,
+    api_base=BASE_URL,
+)
+
 
 def get_product_collection():
-    return _client.get_or_create_collection("product_data")
+    return _client.get_or_create_collection("product_data", embedding_function=_embedding_fn)
 
 
 def get_session_collection():
-    return _client.get_or_create_collection("session_memory")
+    return _client.get_or_create_collection("session_memory", embedding_function=_embedding_fn)
 
 
 def query_collection(collection, query_text: str, n_results: int = 6) -> str:
